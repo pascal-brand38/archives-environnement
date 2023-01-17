2aef1a0d4e4e90093a5f60d83833a225ba9f996a
Pascal Brand <pascal.brand38@gmail.com>
2023-01-17 11:52:53 +0100

    Prepare for routes
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,56 @@
+import {
+  createHashRouter, 
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async';
+
+// pages
 import Climat from "./pages/Climat"
+
+import { Outlet, NavLink } from "react-router-dom";
+
+function RootLayout() {
+  return (
+    <HelmetProvider>
+
+    <div className="pbr-container">
+      <header>
+        <nav className='pbr-menu'>
+          <div className='pbr-menu__logo'>
+            <img src='/src/assets/sun.svg' width="30px"/>
+            <span> Archives Environnement </span>
+          </div>
+          <NavLink to="/">Climat</NavLink>    { /* NavLink has an active to know where we are */ }
+        </nav>
+      </header>
+      <main>
+        <Outlet />    { /* the page of the Route element is displayed */ }
+      </main>
+    </div>
+    </HelmetProvider>
+
+  )
+}
+
+// Use HashRouter instead of BrowserRouter in order to deploy on github pages
+// cf. https://www.freecodecamp.org/news/deploy-a-react-app-to-github-pages/
+const router = createHashRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Climat />} />
+      {/* <Route path="text-effect" element={<TextEffect />} /> */}
+
+      { /* TODO: <Route path="*" element={<NotFound />} /> */ }
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <div className="App">
-      <Climat />
-    </div>
-  )
+    <RouterProvider router={router} />
+  );
 }
 
 export default App
